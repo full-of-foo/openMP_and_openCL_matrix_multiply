99457f2ce8780f8707ae6d8f0d732102e8a8484c
Anthony <lightweightdevelopment@gmail.com>
2015-04-25 00:04:54 +0100

    Added reduction

--- openMP_app/src/mp_mm.c
+++ openMP_app/src/mp_mm.c
@@ -7,13 +7,21 @@ float **matrix_multiply(float **A_p, float **B_p, float **C_p, int aRows, int aC
     int isSquares = aRows == bRows && aCols == bCols;
     int isValidNonSquares = aCols == bRows;
     check(isSquares || isValidNonSquares, "Matrix demensions must agree");
-    int i, j, k;
 
-    #pragma omp parallel for shared(A_p,B_p,C_p,aRows,aCols,bRows) private(i,j,k)
-    for(i=0; i<aRows; i++)
-        for(j=0; j<aCols; j++)
-            for(k=0; k<bRows; k++)
-                C_p[i][j] += (A_p[i][k] * B_p[k][j]);
+    int i, j, k;
+    float sum;
+
+    #pragma omp parallel for shared(A_p,B_p,C_p,aRows,aCols,bRows) private(i,j,k, sum)
+    for(i=0; i<aRows; i++){
+        for(j=0; j<aCols; j++){
+            sum = 0;
+            #pragma omp parallel for reduction(+:sum)
+            for(k=0; k<bRows; k++){
+                sum += (A_p[i][k] * B_p[k][j]);
+            }
+            C_p[i][j] = sum;
+        }
+    }
 
 
     return C_p;
